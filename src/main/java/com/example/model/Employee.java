d31403308d208d5ea70959634ae6b451811a4044
bahadir <bahadiryilmaz135@gmail.com>
2024-04-05 16:21:09 -0400

    Employee creating

---
+++ src/main/java/com/example/model/Employee.java
@@ -0,0 +1,18 @@
+package com.example.model;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+}
