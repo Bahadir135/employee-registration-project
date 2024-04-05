1e3848256da065b81948e0391571cd1942c102b6
bahadir <bahadiryilmaz135@gmail.com>
2024-04-05 17:20:58 -0400

    Front end connecting

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.example.controller;
 
 
+import com.example.bootstrap.DataGenerator;
+import com.example.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController {
 
     @GetMapping("/register")
-    public String createEmployee(){
+    public String createEmployee(Model model){
+
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
+
 
         return "employee/employee-create";
     }
