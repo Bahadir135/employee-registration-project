f2b358133997180cdb521780118ef2103feab758
bahadir <bahadiryilmaz135@gmail.com>
2024-04-10 14:41:37 -0400

    Validation

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -43,7 +43,7 @@ public class EmployeeController {
         }
 
         employeeService.saveEmployee(employee);
-        return "redirect:employee/list";
+        return "redirect:/employee/list";
     }
 
     @GetMapping("/list")
